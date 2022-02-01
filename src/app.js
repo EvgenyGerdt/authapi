499b458d031aa705c95c949d6b3be7bf5170b2ff
Evgeny Gerdt <assmys041298@yandex.ru>
2022-02-02 01:17:57 +0300

    Добавлена документация Swagger

--- src/app.js
+++ src/app.js
@@ -1,25 +1,52 @@
 const express = require('express')
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const config = require('../src/config/baseConfig');
 const connectDatabase = require('../src/config/databaseConfig');
+const SwaggerUI = require('swagger-ui-express');
+const SwaggerJSDoc = require('swagger-jsdoc');
+
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "AuthLearn API",
+            version: "1.0.0",
+            description: "A simple Auth API",
+        },
+        servers: [
+            {
+                url: "http://localhost:3000",
+            },
+            {
+                url: "https://auth-base-learn.herokuapp.com",
+            },
+        ],
+    },
+    apis: ["./src/router/**.js"],
+};
+
+const specs = SwaggerJSDoc(options);
 
 const app = express()
 
-connectDatabase().then(() => {
-    console.log('Connection started');
-});
-
+app.use('/api-docs', SwaggerUI.serve, SwaggerUI.setup(specs));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/api/v1/auth', require('./router/auth.routes'));
+app.use('/api/v1/user', require('./router/user.routes'));
 
 app.get('/', (req, res) => {
     res.send('API for Authentication on Node JS');
 });
 
-app.use('/api/v1/auth', require('./router/auth.routes'));
-app.use('/api/v1/user', require('./router/user.routes'));
-
 const PORT = process.env.PORT || config.PORT;
+
+connectDatabase().then(() => {
+    console.log('Connection started');
+});
 
 app.listen(PORT, () => {
    console.log(`Server is listening at PORT: ${PORT}`);
--- src/router/auth.routes.js
+++ src/router/auth.routes.js
@@ -3,7 +3,143 @@ const router = express.Router();
 
 const { register, login } = require('../controllers/auth.controller');
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       required:
+ *         - email
+ *         - password
+ *         - username
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: Автоматически сгенерированный id
+ *         username:
+ *           type: string
+ *           description: Имя пользователя
+ *         email:
+ *           type: string
+ *           description: Почтовый адрес пользователя
+ *         password:
+ *           type: string
+ *           description: Пароль пользователя
+ *         createdAt:
+ *           type: date
+ *           description: Дата регистрации пользователя
+ *       example:
+ *         username: EvgenyGerdt
+ *         email: test@mail.com
+ *         password: 123Test123
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Authentication
+ *   description: Авторизация и регистрация пользователя
+ */
+
+/**
+ * @swagger
+ * /api/v1/auth/sign-up:
+ *   post:
+ *     summary: Регистрация пользователя
+ *     tags: [Authentication]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       200:
+ *         description: User created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       409:
+ *         description: User already exists
+ *       500:
+ *         description: Internal Server Error
+ */
+
 router.route('/sign-up').post(register);
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Credentials:
+ *       type: object
+ *       required:
+ *         - email
+ *         - password
+ *       properties:
+ *         email:
+ *           type: string
+ *           description: Почтовый адрес пользователя
+ *         password:
+ *           type: string
+ *           description: Пароль пользователя
+ *       example:
+ *         email: test@mail.com
+ *         password: 123Test123
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     SuccessResponse:
+ *       type: object
+ *       required:
+ *         - user
+ *         - token
+ *       properties:
+ *         user:
+ *           type: object
+ *           description: Данные пользователя
+ *         token:
+ *           type: string
+ *           description: Токен пользователя
+ *       example:
+ *         user:
+ *           _id: 21rkoewi2jt010
+ *           email: test@test.com
+ *           password: <HashedPassword>
+ *           createdAt: 12.12.2021
+ *         token: 439-18986gd90wjgjr1opru
+ */
+
+/**
+ * @swagger
+ * /api/v1/auth/sign-in:
+ *   post:
+ *     summary: Авторизация пользователя
+ *     tags: [Authentication]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Credentials'
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/SuccessResponse'
+ *       404:
+ *         description: Invalid credentials
+ *       500:
+ *         description: Internal Server Error
+ */
+
 router.route('/sign-in').post(login);
 
 module.exports = router;
